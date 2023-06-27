861e6e23721db3f175e6089452d6e85a08b1bfcc
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-27 18:41:06 +0500

    Let Var Const

--- Fundamentals/notes.md
+++ Fundamentals/notes.md
@@ -1,3 +1,7 @@
 # Javascript Notes:
 
 i- Value have type not the variable.
+
+Always use const --> let --> var try to avoid var at all cost
+use const as a default
+any mutation in code can lead to potential bugs so try to not or use less let and avoid var
--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -8,3 +8,18 @@ console.log(typeof jSisFun);
 
 let year;
 console.log(typeof year);
+
+//? Let Var Const
+let age = 100;
+age = 12;
+
+const birthDayYear = 1991;
+//! not allowed --> birthDayYear = 2000
+//! not allowed --> const year
+
+var job = "Programmer";
+job = "DBA";
+
+//? let is block scope and var is function scope
+
+//! Never do this --> name = "Ghauoor"
