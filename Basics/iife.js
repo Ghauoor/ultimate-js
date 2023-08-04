e21b1492999ed8bb1c443c3533fe5e029ca09454
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-04 16:30:50 +0500

    Immediately Invoked Function Expressions

---
+++ Basics/iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked function expression (IIFE)
+
+// use to avoid pollution of global scope vars
+
+//names iife
+(function coffee() {
+  console.log("DB Connected");
+})();
+
+(() => {
+  console.log("DB Connected two");
+})();
+((name) => {
+  console.log(`DB Connected to ${name}`);
+})("Ghayoor");
