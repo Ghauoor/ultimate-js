adac812f32d0cf00ca0a16d54cd72bb88a2a4bb4
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-08 12:36:08 +0500

    PropertyDescriptor in Objects

---
+++ Basics/objectMathPi.js
@@ -0,0 +1,25 @@
+const des = Object.getOwnPropertyDescriptor(Math, "PI");
+
+// console.log(des);
+
+const coffee = {
+  name: "IceCoffee",
+  price: 999,
+  isAvailable: true,
+  orderCoffee: function () {
+    console.log("Coffee is not ready yet");
+  },
+};
+console.log(Object.getOwnPropertyDescriptor(coffee, "name"));
+
+Object.defineProperty(coffee, "name", { writable: false, enumerable: false });
+
+// console.log(Object.getOwnPropertyDescriptor(coffee, "name"));
+
+// console.log(coffee);
+
+for (let [key, value] of Object.entries(coffee)) {
+  if (typeof value !== "function") {
+    console.log(`${key}, ${value}`);
+  }
+}
