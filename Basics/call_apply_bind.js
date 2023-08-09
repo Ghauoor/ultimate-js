cbceace7782f44e87d712e1757e7cf25c2d2e8b9
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-09 12:07:58 +0500

    call, apply and bind

---
+++ Basics/call_apply_bind.js
@@ -0,0 +1,40 @@
+printFullName = function (homeTown, state) {
+  return console.log(
+    this.firstName,
+    "",
+    this.lastName,
+    "from",
+    homeTown,
+    ",",
+    state
+  );
+};
+const name = {
+  firstName: "Ghayoor",
+  lastName: "Hussain",
+
+  //! better approach --> keep method outside of the object and then use call().
+  //   printFullName: function () {
+  //     return console.log(this.firstName, "", this.lastName);
+  //   },
+};
+// printFullName.call(name);
+// name.printFullName();
+const nameTwo = {
+  firstName: "Taimoor",
+  lastName: "Hussain",
+};
+// with the call() we can do function borrowing
+// first argu is ref of the this var and than func other funcs with , seprations
+printFullName.call(nameTwo, "Lahore", "Pakistan");
+
+// only diff b/w call() and apply() is the way we pass the arguments
+// in apply method we pass the argument in a list
+
+printFullName.apply(name, ["Lahore", "Pakistan"]);
+
+//bind() look exactly the same as apply method but only diff is the instead calling the method it bind the method with object and return the copy of the object and the main catch is the it return the func so we call it later
+
+let print = printFullName.bind(name, "Lahore", "Pakistan");
+
+print();
