12ebfedb45528c7f5cc3937dbeff2c7e2016bbb1
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-10 16:45:22 +0500

    api req and filteration

---
+++ Basics/bitAdvance/apiReq.js
@@ -0,0 +1,27 @@
+// fetch("https://catfact.ninja/breeds")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+
+//   })
+//   .catch((e) => console.log(e));
+const apiReq = "https://catfact.ninja/breeds";
+const api = async function (api) {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    const filterArray = data.data.filter(function (item) {
+      if (item["origin"] === "Natural" && item["coat"] === "Short") {
+        return true;
+      }
+      return false;
+    });
+    console.log(filterArray);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+api(apiReq);
