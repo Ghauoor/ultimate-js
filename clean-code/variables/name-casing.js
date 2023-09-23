34b338552b7ea284710cf3866c25ca65a3333a0e
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-09-24 01:51:42 +0500

    clean code --variables-names

---
+++ clean-code/variables/name-casing.js
@@ -0,0 +1,12 @@
+//! Snake case
+let first_name = "Ghayoor";
+
+//* camel case ✔
+let firstName = "Ghayoor";
+
+//* Pascal case
+let FirstName = "Ghayoor";
+
+//!  kebab case
+let user = { "first-name": "Ghayoor" };
+user["first-name"];
