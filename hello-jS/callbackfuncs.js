dc7e65fee90998212682795f46b2602904a3ef8d
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-04-28 16:21:48 +0500

    Add example of callback

--- hello-jS/callbackfuncs.js
+++ hello-jS/callbackfuncs.js
@@ -15,6 +15,61 @@ x(function y() {
   console.log("y");
 });
 
+console.log("Before");
+getUser(1, (user) => {
+  console.log("User is", user);
+  callingTheRepo(user.gitHubName, (repos) => {
+    console.log("Repos-->", repos);
+    commitsForUser(user.gitHubName, (commits) => console.log(commits));
+  });
+});
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading data from db");
+    callback({ id: id, gitHubName: "Ghauoor" });
+  }, 1000);
+}
+
+function callingTheRepo(userName, callback) {
+  setTimeout(() => {
+    console.log("Calling the repos");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 1000);
+}
+
+function commitsForUser(userName, callback) {
+  setTimeout(() => {
+    console.log("Calling the repos");
+    callback("Commits");
+  }, 3000);
+}
+
+function fetchData(callback) {
+  setTimeout(() => {
+    const data = { name: "Ghayoor Hussain", gender: "Male" };
+    callback(data);
+  }, 1000);
+}
+
+function processData(data, callback) {
+  setTimeout(() => {
+    data.name = data.name.toUpperCase();
+    callback(data);
+  }, 1000);
+}
+
+function printData(data) {
+  console.log("Data is -->", data);
+}
+
+fetchData(function (data) {
+  processData(data, function (data) {
+    printData(data);
+  });
+});
+
 // function check() {
 //   setTimeout(() => {
 //     console.log("***Inside***");
@@ -28,11 +83,11 @@ x(function y() {
 //? count how many time a button is clicked
 //* 1- use global variable --> not secure, 2- use form a closure
 
-function attachedEventListners() {
-  let count = 0;
-  document.getElementById("btn").addEventListener("click", function xyz() {
-    console.log("Clicked", ++count);
-  });
-}
+// function attachedEventListners() {
+//   let count = 0;
+//   document.getElementById("btn").addEventListener("click", function xyz() {
+//     console.log("Clicked", ++count);
+//   });
+// }
 
-attachedEventListners();
+// attachedEventListners();
